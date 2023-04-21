49575a1e74a9aff0ee966fbac11f45d9ad8f5fdc
tai.le <tai.le@newhorizon-tech.vn>
2023-04-21 09:34:05 +0700

    init

--- src/core/admin/admin.js
+++ src/core/admin/admin.js
@@ -1,42 +1,45 @@
 import { useReducer } from "react";
-import Render from './admin.render'
+import Render from "./admin.render";
 
 const act = {
   GET: "GET",
-  SET: "SET",
-}
+  SET: "SET"
+};
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case act.GET:
-      return { ...state, status: true, count: state.count++ }
+      return { ...state, status: true, count: state.count++ };
     case act.SET:
-      return { ...state, status: false, count: state.count++ }
-    default: return state;
+      return { ...state, status: false, count: state.count++ };
+    default:
+      return state;
   }
-}
+};
 
 const WrapState = (hook) => {
-  const [state, dispatch] = hook
-  return { state, dispatch }
-}
+  const [state, dispatch] = hook;
+  return { state, dispatch };
+};
 
 function Admin() {
-  const vm = WrapState(useReducer(reducer, {
-    data: { name: 'abc' },
-    status: false,
-    count: 0
-  }))
+  const vm = WrapState(
+    useReducer(reducer, {
+      data: { name: "abc" },
+      status: false,
+      count: 0
+    })
+  );
 
   vm.handleSet = () => {
-    vm.dispatch({ type: act.SET })
-  }
+    vm.dispatch({ type: act.SET });
+  };
 
   vm.handleGet = () => {
-    vm.dispatch({ type: act.GET })
-  }
+    vm.dispatch({ type: act.GET });
+  };
 
-  return Render.Admin(vm)
+  return Render.Admin(vm);
 }
 
-export default Admin
+export default Admin;
